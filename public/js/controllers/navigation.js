b738d9cb72153691129f0e5a8c9b76d54793f36e
Renaldo van Dyk <u12204359@tuks.co.za>
2016-04-07 11:56:04 +0200

    Fixed Headers working 100%

--- public/js/controllers/navigation.js
+++ public/js/controllers/navigation.js
@@ -7,6 +7,7 @@ app.controller('navControl',function($scope, authService, session, $location, $t
     // Set header message of signup $ login pages
     $timeout(function(){
         if($location.path() == "/signUp" || $location.path() == "/") {
+            $scope.slog = true;
             $scope.slog1 = "Today's Talent.";
             $scope.slog2 = "Tomorrow's Success."
         }
@@ -14,55 +15,78 @@ app.controller('navControl',function($scope, authService, session, $location, $t
 
     if(authService.isAuthenticated()){
         var user = session.user;
-
         if(user.type == "student")
         {
             // Set header message of Dash
-
+            $scope.slog1 = "";
+            $scope.slog2 = "";
+            $timeout(function() {
                 if ($location.path() == "/dashboard") {
-                    $timeout(function() {
                     $scope.welcome = "Welcome ";
                     $scope.talent = user.name.name + "!";
-
-            });
                 }
-
+            });
             $scope.getNav= function() {
+
                 return "../views/blocks/studentNav.html";
             }
         }
         else if(user.type == "employer"){
-
             // Set header message of Dash
-            if ($location.path() == "/dashboard") {
-                $timeout(function () {
+            $scope.slog1 = "";
+            $scope.slog2 = "";
+            $timeout(function () {
+                if ($location.path() == "/dashboard") {
                     $scope.welcome = "Welcome ";
                     if (!user.company)
                         $scope.employer = user.contact.name + "!";
                     else
                         $scope.employer = user.company.name + "!";
 
-                });
-            }
-
+                }
+            });
             $scope.getNav= function() {
                 return "../views/blocks/employerNav.html";
             }
         }
     }
-    else if($location.path() != "/" && $location.path() != "/signIn" && $location.path() != "/signUp")  {
+    else if($location.path() != "/" && $location.path() != "/signIn" && $location.path() != "/signUp" && $location.path() != "/activate")  {
         swal({   title: "Log in first",   type: "error",   timer: 2000,   showConfirmButton: false });
         $location.url("/signIn")
     }
 
     $scope.$on('auth-login-success',function(){
         var user = session.user;
+
         if(user.type == "student")
         {
+            // Set header message of Dash
+            $scope.slog1 = "";
+            $scope.slog2 = "";
+            $timeout(function() {
+                if ($location.path() == "/dashboard") {
+                    $scope.welcome = "Welcome ";
+                    $scope.talent = user.name.name + "!";
+                }
+            });
             $scope.getNav= function() {
                 return "../views/blocks/studentNav.html";
-            }}
+            }
+        }
         else if(user.type == "employer"){
+            // Set header message of Dash
+            $scope.slog1 = "";
+            $scope.slog2 = "";
+            $timeout(function () {
+                if ($location.path() == "/dashboard") {
+                    $scope.welcome = "Welcome ";
+                    if (!user.company)
+                        $scope.employer = user.contact.name + "!";
+                    else
+                        $scope.employer = user.company.name + "!";
+
+                }
+            });
             $scope.getNav= function() {
                 return "../views/blocks/employerNav.html";
             }
@@ -79,6 +103,13 @@ app.controller('studentNav',function($scope,$rootScope, $window, session, authSe
 
     var user = session.user;
     $scope.user = user;
+
+    if ($location.path() == "/dashboard") {
+        $timeout(function() {
+            $scope.welcome = "Welcome ";
+            $scope.talent = user.name.name + "!";
+        });
+    }
 
     $http.post('/getPp', user)
         .then(function (res) {
