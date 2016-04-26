cd82b7aab11c222755e57a9bbaa07fa78b0cf24d
Renaldo van Dyk <u12204359@tuks.co.za>
2016-04-26 13:11:32 +0200

    Signup styling

--- public/js/helpers/userForms.js
+++ public/js/helpers/userForms.js
@@ -1,4 +1,18 @@
 ////Renaldo se user form scripts, uit sodat dit makliker is om te edit(gebruik vir edit profile ook)
+if (document.addEventListener) {
+    document.addEventListener('invalid', function(e) {
+        var element = $(e.target);
+        element.addClass("invalid");
+    }, true);
+}
+$(document).bind('keypress', function(e){
+
+    if( $(e.target).is(':invalid') ){
+        $(e.target).addClass('invalid');
+    } else {
+        $(e.target).removeClass('invalid');
+    }
+});
 
 $(function() {
     var dob;
